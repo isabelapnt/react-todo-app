885274608dd24ecf8aca12ec96ae350ab6cd91c9
Isabela Pinto <isabelasouzapinto@gmail.com>
2018-09-25 11:05:11 -0300

    add server and loader settings to run in dev mode

---
+++ backend/src/config/server.js
@@ -0,0 +1,13 @@
+const port = 3003
+
+const bodyParser = require('body-parser')
+const express = require('express')
+const server = express()
+
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
+
+server.listen(port, function(){
+    console.log(`everything is fine :)`)
+    console.log(`backend is running on port ${port}.`)
+})
