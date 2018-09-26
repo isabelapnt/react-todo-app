e52c38df78f57742b24678a7e6008bd9177dabc7
Isabela Pinto <isabelasouzapinto@gmail.com>
2018-09-26 09:44:58 -0300

    created menu component

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,10 +1,14 @@
 import 'modules/bootstrap/dist/css/bootstrap-theme.min.css'
 //import '../static/font-awesome.min.css'
-
 import React from 'react'
+import Todo from '../todo/todo'
+import About from '../about/about'
+import Menu from '../template/menu'
 
 export default props => (
     <div className='container'>
-        <h1>Testes</h1>
+        <Menu />
+        <Todo />
+        <About />
     </div>
 )
