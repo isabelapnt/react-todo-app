9ebee705643f3199c3979c77737c44cf64327cd9
Isabela Pinto <isabelasouzapinto@gmail.com>
2018-09-26 10:19:39 -0300

    page header component and text to about

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
     <div>
-        <h1>About</h1>
+        <PageHeader name='About' small="we" />
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse laoreet semper ultrices. Integer a ultricies odio. Donec dapibus velit et tortor pharetra aliquam. Proin consequat sodales dui id porta. Etiam nec ex sed sapien tristique finibus. Ut at consectetur elit. Pellentesque quis vulputate felis, vitae sagittis metus. Cras in nisi in felis sagittis auctor quis id odio. Nulla at urna pharetra nisl vestibulum vestibulum sed vitae massa. Suspendisse in augue ac risus consequat hendrerit aliquam in nunc.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse laoreet semper ultrices. Integer a ultricies odio. Donec dapibus velit et tortor pharetra aliquam. Proin consequat sodales dui id porta. Etiam nec ex sed sapien tristique finibus. Ut at consectetur elit. Pellentesque quis vulputate felis, vitae sagittis metus. Cras in nisi in felis sagittis auctor quis id odio. Nulla at urna pharetra nisl vestibulum vestibulum sed vitae massa. Suspendisse in augue ac risus consequat hendrerit aliquam in nunc
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse laoreet semper ultrices. Integer a ultricies odio. Donec dapibus velit et tortor pharetra aliquam. Proin consequat sodales dui id porta. Etiam nec ex sed sapien tristique finibus. Ut at consectetur elit. Pellentesque quis vulputate felis, vitae sagittis metus. Cras in nisi in felis sagittis auctor quis id odio. Nulla at urna pharetra nisl vestibulum vestibulum sed vitae massa. Suspendisse in augue ac risus consequat hendrerit aliquam in nunc
+        </p>
     </div>
 )
