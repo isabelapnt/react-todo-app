6e5a6a4d428335cbf317967438bfe355612b9f10
Isabela Pinto <isabelasouzapinto@gmail.com>
2018-09-28 14:37:07 -0300

    redux settings

---
+++ frontend/src/todo/todoReducers.js
@@ -0,0 +1,31 @@
+const INITIAL_STATE = {
+    description: 'read books',
+    list:[
+        {
+            _id: 1,
+            description: 'task 1',
+            done: true
+        },
+        {
+            _id: 2,
+            description: 'task 2',
+            done: false
+        },
+        {
+            _id: 3,
+            description: 'task 3',
+            done: true
+        }
+    ]
+}
+
+export default (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case 'DESCRIPTION_CHANGED':
+            return { ...state, description: action.payload }
+        case 'TODO_SEARCHED':
+            return { ...state, list: action.payload }
+        default:
+            return state
+    }
+}
